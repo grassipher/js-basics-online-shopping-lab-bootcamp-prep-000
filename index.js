157f1b217212b770918204ea599b06721128e4b4
Gabriel Valle <gvalle91@gmail.com>
2018-05-28 15:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     var output = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-    cart.splice(0,cart.length-1);
+    cart.splice(0,cart.length);
     return output;
   }
 }
