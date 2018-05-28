a3a19634027b199e50179612bd916c562b566f89
Gabriel Valle <gvalle91@gmail.com>
2018-05-28 15:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (carNumber isNaN){
+  if (cardNumber isNaN){
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     var output = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
