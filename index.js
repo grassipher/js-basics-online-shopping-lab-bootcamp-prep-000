da028b56bdc2b4a8fae91bd2d625bceeb776b114
Gabriel Valle <gvalle91@gmail.com>
2018-05-28 15:37:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,6 +64,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === ""){
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    
+    var output = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`;
+    cart.splice(0,cart.length-1);
+    return output;
   }
 }
